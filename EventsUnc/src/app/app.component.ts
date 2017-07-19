a53da0c7bb6fc3ee008827dceb6fd3a39f131172
gero <gero@blacksmith.gerardo>
2017-07-19 12:20:55 -0300

    Implementacion de momentjs

--- EventsUnc/src/app/app.component.ts
+++ EventsUnc/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  hoy=moment().locale('es').format('LLLL');
+  
 }
