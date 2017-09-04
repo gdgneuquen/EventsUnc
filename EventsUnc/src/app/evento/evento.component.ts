d63f7b47129f0aefb3d76f1c685ac9e40f473633
Elpagano <estejpagano@gmail.com>
2017-09-04 08:42:05 -0300

    antes de arreglar filtros

--- EventsUnc/src/app/evento/evento.component.ts
+++ EventsUnc/src/app/evento/evento.component.ts
@@ -12,7 +12,9 @@ import { AuthService } from '../providers/auth.service';
 
 import * as firebase from 'firebase/app';
 import * as moment from 'moment';
+import * as _ from 'lodash';
 
+import {Evento} from "../commons/evento.model"
 
 @Component({
   selector: 'app-evento',
@@ -29,53 +31,57 @@ export class EventoComponent  implements OnInit {
   filtroDia = moment().locale('es').format('YYYY-MM-DD'); //formato firebase
   dia = moment().locale('es').format('dddd');//dia de la semana
 
-  //Relacionado al plugin ngx-order-pipe. Hay que agregarlo a imports en app.module.ts
-  order = "actividad.pickerDesde";
-  reverse = true;
-
   //Relacionado a angularfire2
-  user: Observable<firebase.User>;
-  actividades: FirebaseListObservable<any[]>; //actividades es tipo any para poder recibir todo lo que le trae el servicio
-  msgVal: string = ''; //mensaje de entrada del form
-  selectedActividad: string = '';
-  hora=0;
-
-  
-  constructor(
-    private authService: AuthService,
+   actividades: FirebaseListObservable<any[]>; //actividades es tipo any para poder recibir todo lo que le trae el servicio
+    
+  constructor( 
     public af: AngularFireDatabase,
-    private router: Router) {}
+   // public ev: Evento
+  ) {}
+/*
+//--datos para filtrar
+  /// unwrapped arrays from firebase
+  actividades: any;
+  filteredActividades: any;
+  /// filter-able properties
+  family:     string;
+  weight:     number;
+  endangered: boolean;
+  /// Active filter rules
+  filters = {}
+ 
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(1, 5));
+  } 
+  */
+
+  ngOnInit(): void {
+
     this.getActividades();
-    console.log(
-      "horaActual: "+this.horaActual +"\n"+
-      "hoy: "+this.hoy +"\n"+
-      "filtroDia: "+this.filtroDia +"\n"+
-      "dia: "+this.dia
-    );
+
+    this.actividades.subscribe(items => {
+      // items is an array
+      this.actividades.forEach(actividades => {
+          console.log('actividades:', actividades);
+      });      
+  });
+
   }
 
-  isUserLoggedIn(){
-     return this.authService.loggedIn;
-   }
-   
   getActividades(){
-      //Es necesario que las actividades tengan un campo fechaInicio para que se muestren las actividades del dia
       //En otro componente los valores de query no deben tener orderByChild:'fechaInicio', equalTo:hoy, sino 'startAt'.
       //this.actividades = this.af.list('/actividades');
-      
+
     this.actividades = this.af.list('/actividades',
       {
         query: {
           orderByChild: 'pickerDesde',
-         // endAt: '2017-09-19',
-         // endAt: this.filtroDia
-          //equalTo: this.hoy
+          limitToFirst: (3)
         }
       });
   }
-
   //esta funcion estaba pensada paracambiar el fondo por uno mas llamativo de la actividad en curso.
   estaEnCurso(horaInicio, horaFin) {
     // TODO: implementar funcion.
