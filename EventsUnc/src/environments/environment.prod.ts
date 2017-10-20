07bce4d36eb2a6e9a75396fa80044ffc467d2e8c
Seba Herrera Cano <sebaynil@gmail.com>
2017-10-20 18:01:22 -0300

    fixes para deployar la App

--- EventsUnc/src/environments/environment.prod.ts
+++ EventsUnc/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: "AIzaSyBPX5UnZYgccgbqrAipQOhsc8chEUbRwVc",
+    authDomain: "faea-eventos.firebaseapp.com",
+    databaseURL: "https://faea-eventos.firebaseio.com",
+    projectId: "faea-eventos",
+    storageBucket: "faea-eventos.appspot.com",
+    messagingSenderId: "966324489874"
+  }
 };
