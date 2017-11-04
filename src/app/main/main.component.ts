698b84e89356f9c6233093dd34f671bad239c795
G33N <mceliz@enlaceit.com.ar>
2017-11-04 00:16:06 -0300

    Add filter to main page
    I had to modify the header component to apply the filter in the main component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,6 +2,7 @@ import { AuthService } from '../providers/auth.service';
 import { Component, NgZone, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 //import 'rxjs/add/observable/throw';//Para trabajar con los observables desde rxjs
 //import 'rxjs/add/operator/catch';//para poder tomar cosas
 //import 'rxjs/add/operator/toPromise';
@@ -21,33 +22,53 @@ export class MainComponent implements OnInit {
 
   hoy = moment().locale('es').format('LLLL');
   //actividades es tipo any para poder recibir todo lo que le trae el servicio
-  actividades:FirebaseListObservable<any[]>;
+  actividades: FirebaseListObservable<any[]>;
+  items: any; // AUX to filter actividades
   msgVal: string = ''; //mensaje de entrada del form
   selectedActividad: string = '';
+  test: any;
+  constructor(private authService: AuthService, public af: AngularFireDatabase, private router: Router) {
 
-  constructor(
-    private authService: AuthService,
-    public af: AngularFireDatabase,
-    private router: Router){}
+  }
 
-  ngOnInit(){
-    this.actividades = this.af.list('/actividades', { query: { limitToLast: 50 } });
+  ngOnInit() {
+    this.getActivities();
     //      this.actividades.push({ horario: "8:00 a 9:00"});
   }
-  isUserLoggedIn(){
-     return this.authService.loggedIn;
+  isUserLoggedIn() {
+    return this.authService.loggedIn;
   }
 
-  deleteActivity(key):void {
-      //alert("la Fecha inicio y hora inicio tienen que estar llennas");
-      if(confirm("Esta seguro que desea borrar el evento?")) {
-        this.actividades.remove( key);
-        this.msgVal = '';
-      }
+  getActivities() {
+    this.actividades = this.af.list('/actividades');
+    this.actividades.subscribe( snapshot => {
+      this.items = snapshot;
+    });
   }
 
-  modificarActivity(key){
+  deleteActivity(key): void {
+    //alert("la Fecha inicio y hora inicio tienen que estar llenas");
+    if (confirm("Esta seguro que desea borrar el evento?")) {
+      this.actividades.remove(key);
+      this.msgVal = '';
+    }
+  }
+
+  modificarActivity(key) {
     this.router.navigate(['/modEvento', key]);
+  }
+
+  searchActivities(ev: any) {
+    // Reset items back to all of the items
+    this.getActivities();
+    // set val to the value of the searchbar
+    let val = ev;
+    // if the value is an empty string don't filter the items. This filter items by descripcion or nombre properties
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.descripcion.toLowerCase().indexOf(val.toLowerCase()) > -1 || item.nombre.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      });
+    }
   }
 
 }
